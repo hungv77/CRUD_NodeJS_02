01e620fedffa8e65c1ef242faf4c557c3ddf9fba
hungv77 <kogome6a@gmail.com>
2024-02-22 17:43:02 +0700

    Another#1 Commit

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,4 +1,5 @@
 import express from "express";
+import homeController from '../controller/homeController';
 
 const router = express.Router();
 
@@ -8,10 +9,9 @@ const router = express.Router();
  */
 
 const initWebRoutes = (app) => {
-  router.get("/", (req,res)=>{
-    return res.send("Hello world");
-  })
+  router.get("/", homeController.handleHelloWorld)
 
+  router.get("/user", homeController.handleUserPage)
   return app.use("/", router);
 }
 
