dfe471d074ecb177f011d3928dc76456703fe7ba
hungv77 <kogome6a@gmail.com>
2024-02-23 02:21:29 +0700

    second commit

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,11 +1,26 @@
-const handleHelloWorld = (req,res) => {
-  return res.render("home.ejs");
-}
+import userService  from '../service/userService';
 
-const handleUserPage = (req,res) => {
+const handleHelloWorld = (req, res) => {
+  return res.render("home.ejs");
+};
+
+const handleUserPage = (req, res) => {
   return res.render("user.ejs");
-}
+};
+
+const handleCreateNewUser = (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let username = req.body.username;
+
+  // userService.createNewUser(email, password, username)
+
+  userService.getUserList();
+  return res.send("handleCreateNewUser");
+};
 
 module.exports = {
-  handleHelloWorld,handleUserPage
-}
+  handleHelloWorld,
+  handleUserPage,
+  handleCreateNewUser,
+};
