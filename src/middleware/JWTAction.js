190e5466bcacaaef0373d802af053c1a45b49fe5
hungv77 <kogome6a@gmail.com>
2024-04-03 00:21:00 +0700

    #18.2 Loading, Fix

--- src/middleware/JWTAction.js
+++ src/middleware/JWTAction.js
@@ -2,14 +2,16 @@ require("dotenv").config();
 
 import jwt from "jsonwebtoken";
 
-const nonSecurePaths = ['/', '/login', '/register'];
+const nonSecurePaths = ["/", "/login", "/register"];
 
 const createJWT = (payload) => {
   let key = process.env.JWT_SECRET;
   let token = null;
 
   try {
-    token = jwt.sign(payload, key);
+    token = jwt.sign(payload, key, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
   } catch (err) {
     console.log(err);
   }
@@ -30,65 +32,66 @@ const verifyToken = (token) => {
 };
 
 const checkUserJWT = (req, res, next) => {
-  if(nonSecurePaths.includes(req.path)) return next(); 
+  if (nonSecurePaths.includes(req.path)) return next();
 
   let cookies = req.cookies;
-  if(cookies && cookies.jwt){
+  if (cookies && cookies.jwt) {
     let token = cookies.jwt;
     let decoded = verifyToken(token);
-    if(decoded){
+    if (decoded) {
       req.user = decoded;
       req.token = token;
       next();
     } else {
       return res.status(401).json({
         EC: -1,
-        DT: '',
-        EM: 'Not authenticated the user'
-      })
+        DT: "",
+        EM: "Not authenticated the user",
+      });
     }
   } else {
     return res.status(401).json({
       EC: -1,
-      DT: '',
-      EM: 'Not authenticated the user'
-    })
+      DT: "",
+      EM: "Not authenticated the user",
+    });
   }
-}
+};
 
 const checkUserPermission = (req, res, next) => {
-  if(nonSecurePaths.includes(req.path) || req.path === '/account') return next(); 
+  if (nonSecurePaths.includes(req.path) || req.path === "/account")
+    return next();
 
-  if(req.user){
+  if (req.user) {
     let email = req.user.email;
     let roles = req.user.groupWithRoles.Roles;
     let currentUrl = req.path;
-    
-    if(!roles || roles.length === 0){
+
+    if (!roles || roles.length === 0) {
       return res.status(403).json({
         EC: -1,
-        DT: '',
-        EM: "You don't have permission to access this resource :)"
-      })
+        DT: "",
+        EM: "You don't have permission to access this resource :)",
+      });
     }
-    let canAccess = roles.some(item => item.url === currentUrl);
-    if(canAccess === true){
+    let canAccess = roles.some((item) => item.url === currentUrl);
+    if (canAccess === true) {
       next();
     } else {
       return res.status(403).json({
         EC: -1,
-        DT: '',
-        EM: "You don't have permission to access this resource 2 :)"
-      })
+        DT: "",
+        EM: "You don't have permission to access this resource 2 :)",
+      });
     }
-  }else {
+  } else {
     return res.status(401).json({
       EC: -1,
-      DT: '',
-      EM: 'Not authenticated the user'
-    })
+      DT: "",
+      EM: "Not authenticated the user",
+    });
   }
-}
+};
 
 module.exports = {
   createJWT,
